283970e58bf78f965a57d69dc5038fa380d8c170
CHATHURALIYANAGE1997 <chathurabimalka@gmail.com>
2022-07-15 12:31:45 +0530

    Add files

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,8 +1,26 @@
+import React from 'react';
+import {BrowserRouter, BrowserRouter as Routes, Route} from 'react-router-dom';
+import Login from './pages/Login'
+
+
 function App() {
   return (
     <div>
-      Hello world!
+      <BrowserRouter>
+        <Routes>
+                <div className="text-center">
+                    <Route> 
+                          <Route path = "/" exact component = {Login}></Route>
+                          <Route path = "/login"  component = {Login}></Route>
+
+                           {/*<Route path = "/home" exact component = {}></Route> */}
+                    </Route>
+                </div>
+              
+        </Routes>
+        </BrowserRouter>
     </div>
+    
   );
 }
 
