612264d1958787d058906b56ac0a4f9511bbbb69
Ävishi <avijayaweera1@gmail.com>
2022-07-20 01:02:54 +0530

    Making the Sign up ui with all the possible functionalities for the fields

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,6 +3,7 @@ import {BrowserRouter, BrowserRouter as Routes, Route} from 'react-router-dom';
 import HotelInfo from './components/Hotels/HotelInfo';
 import Hotels from './pages/Hotels';
 import Login from './pages/Login'
+import Signup from './pages/Signup'
 import Welcomepage from './pages/Welcomepage';
 
 
@@ -15,6 +16,7 @@ function App() {
                     <Route> 
                           <Route path = "/" exact component = {Login}></Route>
                           <Route path = "/login"  component = {Login}></Route>
+                          <Route path = "/signup"  component = {Signup}></Route>
                           <Route path = "/welcome"  component = {Welcomepage}></Route>
                           <Route path = "/hotels"  component = {Hotels}></Route>
                           <Route path = "/hotel/"  component = {HotelInfo}></Route>
