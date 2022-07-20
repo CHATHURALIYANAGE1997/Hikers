51a0e3f2801d2ca52ef9f59ef65b3ade308451da
ShiranthaKellum <skellumjayasooriya@gmail.com>
2022-07-20 10:48:53 +0530

    Set of changes.

--- front-end/src/pages/landing.jsx
+++ front-end/src/pages/landing.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import Navbar from "../components/Navbar/Navbar"
+// import Navbar from "../components/Navbar/Navbar"
+import "../Styles/landingPage.css"
 
 const LandingPage = () => {
     return (
