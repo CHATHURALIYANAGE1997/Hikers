1811d5f20ac5bc88f68e9f6e95e6fc65980ed049
Sathya Karunakalage <sathyaudayangi2@gmail.com>
2022-07-23 07:10:51 +0530

    Add Hotel Facilities Component

---
+++ front-end/src/pages/HotelFacilities.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import HotelNav from "../components/Hotels/HotelNav";
+import Navbar from "../components/Navbar/Navbar";
+import "../components/Hotels/facilities.css";
+import epitome from "../components/Hotels/Epitome.png";
+import resturant from "../components/Hotels/resturant.png";
+import hotelview from "../components/Hotels/hotelview.png";
+import food from "../components/Hotels/food.png";
+import pool from "../components/Hotels/pool.png";
+import Facilities from "../components/Hotels/Facilities";
+
+
+
+const HotelFacilities = () => {
+    return (
+        <div>
+            <Navbar />
+            <div class="p-5 mt-3 text-left container">
+                <h2 className="mb-3 title font-weight-bold hotel-name">The Epitome</h2>
+                <div className="d-flex flex-row hf-page-container">
+                    <div className="d-flex flex-column hf-left-side">
+                        <div className="d-flex flex-row">
+                            <img src={epitome} className="float-left hf-h-img" alt="front view"></img>
+                            <img src={resturant} className="float-left hf-h-img" alt="resturant"></img>
+                        </div>
+                        <div className="d-flex flex-row">
+                            <img src={hotelview} className="float-left hf-h-img" alt="hotel view"></img>
+                            <img src={food} className="float-left hf-h-img" alt="food"></img>
+                        </div>
+                        <div className="w-100">
+                            <img src={pool} className="float-left hf-pool-img" alt="food"></img>
+                        </div>
+                    </div>
+                    <div className=" d-flex flex-column hf-right-side">
+                        <HotelNav />
+                        <Facilities />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default HotelFacilities;
--- front-end/src/pages/HotelInfo.jsx
+++ front-end/src/pages/HotelInfo.jsx
@@ -22,10 +22,10 @@ const HotelInfo = () => {
                             </div>
                         </div>
                         <div className="d-flex flex-row l-container">
-                            <div className="mt-2 w-50 bg-light location">
-
+                            <div className="mt-2 bg-light location">
+                                Map
                             </div>
-                            <div className="mt-2 ml-2 pt-2 pb-2 pl-3 pr-3 w-50 book-info">
+                            <div className="w-50 mt-2 ml-2 pt-2 pb-2 pl-3 pr-3 book-info">
                                 <h6 className="font-weight-bold">District</h6>
                                 <div className="bg-light text-center mb-2 text-muted data">Kurunegala</div>
                                 <h6 className="font-weight-bold">Check-In date</h6>
@@ -51,30 +51,28 @@ const HotelInfo = () => {
                                 <tbody>
                                     <tr>
                                         <td>
-                                            <div className="font-weight-bold">Standard Single Room</div>
+                                            <div className="bed-type">Standard Single Room</div>
                                             <div className="d-flex flex-column">
-                                                <span>1 Single bed</span>
-                                                <span>Free Wifi</span>
-                                                <span>Breakfast included</span>
+                                                <span className="r-facility">1 Single bed</span>
+                                                <span className="r-facility">Free Wifi</span>
+                                                <span className="r-facility">Breakfast included</span>
                                             </div>
                                         </td>
                                         <td>1</td>
                                         <td >
                                             <div className="d-flex flex-column">
-                                                <span className="font-weight-bold">LKR 5000.00</span>
-                                                <span>+ 10% Taxes and charges</span>
+                                                <span className="r-price">LKR 5000.00</span>
+                                                <span className="r-facility">+ 10% Taxes and charges</span>
                                             </div>
                                         </td>
                                         <td>
                                             <div class="dropdown">
-                                                <button class="btn  dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                                    0
-                                                </button>
-                                                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                                                    <a class="dropdown-item" href="#">Action</a>
-                                                    <a class="dropdown-item" href="#">Another action</a>
-                                                    <a class="dropdown-item" href="#">Something else here</a>
-                                                </div>
+                                                <select className="rooms" name="room">
+                                                    <option value="0">0</option>
+                                                    <option value="1">1</option>
+                                                    <option value="2">2</option>
+                                                    <option value="3">3</option>
+                                                </select>
                                             </div>
                                         </td>
                                         <td>
@@ -83,43 +81,60 @@ const HotelInfo = () => {
                                     </tr>
                                     <tr>
                                         <td>
-                                            <div className="font-weight-bold">Standard Double Room</div>
+                                            <div className="bed-type">Standard Double Room</div>
                                             <div className="d-flex flex-column">
-                                                <span>1 Double bed</span>
-                                                <span>Free Wifi</span>
-                                                <span>Breakfast included</span>
-                                            </div>
-                                        </td>
-                                        <td>2</td>
-                                        <td >
-                                            <div className="d-flex flex-column">
-                                                <span className="font-weight-bold">LKR 10000.00</span>
-                                                <span>+ 10% Taxes and charges</span>
+                                                <span className="r-facility">1 Double bed</span>
+                                                <span className="r-facility">Free Wifi</span>
+                                                <span className="r-facility">Breakfast included</span>
                                             </div>
                                         </td>
                                         <td>1</td>
+                                        <td >
+                                            <div className="d-flex flex-column">
+                                                <span className="r-price">LKR 10000.00</span>
+                                                <span className="r-facility">+ 10% Taxes and charges</span>
+                                            </div>
+                                        </td>
+                                        <td>
+                                            <div class="dropdown">
+                                                <select className="rooms" name="room">
+                                                    <option value="0">0</option>
+                                                    <option value="1">1</option>
+                                                    <option value="2">2</option>
+                                                    <option value="3">3</option>
+                                                </select>
+                                            </div>
+                                        </td>
                                         <td>
                                             <a href="#"><button className="btn btn-primary">Reserve</button></a>
                                         </td>
                                     </tr>
                                     <tr>
                                         <td>
-                                            <div className="font-weight-bold">Standard Thriple Room</div>
+                                            <div className="bed-type">Standard Tripple Room</div>
                                             <div className="d-flex flex-column">
-                                                <span>1 Single bed</span>
-                                                <spa>1 Double bed</spa>
-                                                <span>Free Wifi</span>
-                                                <span>Breakfast & dinner included</span>
+                                                <span className="r-facility">1 Single bed & 1 Double bed</span>
+                                                <span className="r-facility">Free Wifi</span>
+                                                <span className="r-facility">Breakfast included</span>
                                             </div>
                                         </td>
                                         <td>1</td>
                                         <td >
                                             <div className="d-flex flex-column">
-                                                <span className="font-weight-bold">LKR 15000.00</span>
-                                                <span>+ 10% Taxes and charges</span>
+                                                <span className="r-price">LKR 14000.00</span>
+                                                <span className="r-facility">+ 10% Taxes and charges</span>
                                             </div>
                                         </td>
-                                        <td>3</td>
+                                        <td>
+                                            <div class="dropdown">
+                                                <select className="rooms" name="room">
+                                                    <option value="0">0</option>
+                                                    <option value="1">1</option>
+                                                    <option value="2">2</option>
+                                                    <option value="3">3</option>
+                                                </select>
+                                            </div>
+                                        </td>
                                         <td>
                                             <a href="#"><button className="btn btn-primary">Reserve</button></a>
                                         </td>
