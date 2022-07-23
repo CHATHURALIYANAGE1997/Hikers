bf91736a01dfeb4b52e9e9e283bef2ec2e6800dc
Sathya Karunakalage <sathyaudayangi2@gmail.com>
2022-07-23 13:19:11 +0530

    Change Path

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {BrowserRouter, BrowserRouter as Routes, Route} from 'react-router-dom';
-import HotelInfo from './components/Hotels/HotelInfo';
+import HotelInfo from './pages/HotelInfo';
 import Hotels from './pages/Hotels';
 import Login from './pages/Login'
 import Signup from './pages/Signup'
@@ -8,6 +8,7 @@ import Signin from './pages/Signin'
 import Welcomepage from './pages/Welcomepage';
 import LandingPage from './pages/landing';
 import Articles from './pages/Articles';
+import HotelFacilities from './pages/HotelFacilities';
 
 
 function App() {
@@ -23,7 +24,8 @@ function App() {
                           <Route path = "/signin"  component = {Signin}></Route>
                           <Route path = "/welcome"  component = {Welcomepage}></Route>
                           <Route path = "/hotels"  component = {Hotels}></Route>
-                          <Route path = "/hotel/"  component = {HotelInfo}></Route>
+                          <Route path = "/hotel"  component = {HotelInfo}></Route>
+                          <Route path = "/facilities"  component = {HotelFacilities}></Route>
                           <Route path = "/landingPage/" component= {LandingPage}></Route>
                           <Route path = "/articles/" component= {Articles}></Route>
 
