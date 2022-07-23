3c8047c4c41c8587ee1677318dded20e196df9eb
Sathya Karunakalage <sathyaudayangi2@gmail.com>
2022-07-23 07:05:23 +0530

    Change Hotel Nav Bar Color

--- front-end/src/components/Hotels/HotelNav.jsx
+++ front-end/src/components/Hotels/HotelNav.jsx
@@ -4,14 +4,14 @@ import hotelinfo from "./hotelnav.css";
 const HotelNav = () => {
     return (
         <div>
-            <div className="d-flex flex-column right-side">
-                <div className="btn-group w-100 mb-2">
-                    <button className="btn btn-info font-weight-bold w-25 header2">Hotel Info & Price</button>
-                    <button className="btn btn-info font-weight-bold w-25 header2">Facilities</button>
-                    <button className="btn btn-info font-weight-bold w-25 header2">Hotel Rules</button>
-                    <button className="btn btn-info font-weight-bold w-25">Guest Reviews</button>
-                </div>
+            <div className="d-flex flew-row w-100 mb-2">
+                <div className="w-25 hn-bg">Hotel Info & Price</div>
+                <div className="w-25 hn-bg">Facilities</div>
+                <div className="w-25 hn-bg">Hotel Rules</div>
+                <div className="w-25 hn-bg2">Guest Reviews</div>
+
             </div>
+
         </div>
     );
 }
