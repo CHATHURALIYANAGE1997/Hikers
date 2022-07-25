ad38b9e4b7c4659e99aa4c7e3d6e7a132a8fa374
CHATHURALIYANAGE1997 <chathurabimalka@gmail.com>
2022-07-25 22:22:32 +0530

    Update Hutils.java

--- back-end/src/main/java/Hikers/Hikers/utils/Hutils.java
+++ back-end/src/main/java/Hikers/Hikers/utils/Hutils.java
@@ -1,8 +1,15 @@
 package Hikers.Hikers.utils;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
 public class Hutils {
-    
+
     private Hutils() {
+    }
+
+    public static ResponseEntity<String> getResponseEntity(){
+        return  new ResponseEntity<String>("message", HttpStatus.ACCEPTED);
     }
 
 }
