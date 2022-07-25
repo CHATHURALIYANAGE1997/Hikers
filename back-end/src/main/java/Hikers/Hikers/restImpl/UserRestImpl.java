ec8035122ae049d04b4705be2fc7a0709fdcb2ff
CHATHURALIYANAGE1997 <chathurabimalka@gmail.com>
2022-07-25 22:44:35 +0530

    Add files via upload

--- back-end/src/main/java/Hikers/Hikers/restImpl/UserRestImpl.java
+++ back-end/src/main/java/Hikers/Hikers/restImpl/UserRestImpl.java
@@ -7,5 +7,5 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController("/user")
 public class UserRestImpl implements UserRest {
-    
+
 }
