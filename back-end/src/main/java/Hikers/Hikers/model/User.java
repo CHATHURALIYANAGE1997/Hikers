c0527c2c118e69705ddc34ff2a8038c799752848
CHATHURALIYANAGE1997 <chathurabimalka@gmail.com>
2022-07-25 22:48:17 +0530

    Update User.java

--- back-end/src/main/java/Hikers/Hikers/model/User.java
+++ back-end/src/main/java/Hikers/Hikers/model/User.java
@@ -1,6 +1,22 @@
 package Hikers.Hikers.model;
 
+import lombok.Data;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+import javax.persistence.*;
 import java.io.Serializable;
 
+
+@Data
+@Entity
+@DynamicUpdate
+@DynamicInsert
+@Table(name = "user")
 public class User implements Serializable{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private Integer id;
 }
