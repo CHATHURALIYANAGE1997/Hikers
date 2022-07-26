4677a7fe55449a9d7d38e599cbfec986a9ad6864
CHATHURALIYANAGE1997 <chathurabimalka@gmail.com>
2022-07-26 20:32:54 +0530

    Update UserRepo.java

--- back-end/src/main/java/Hikers/Hikers/repository/UserRepo.java
+++ back-end/src/main/java/Hikers/Hikers/repository/UserRepo.java
@@ -2,8 +2,15 @@ package Hikers.Hikers.repository;
 
 import Hikers.Hikers.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface UserRepo  extends JpaRepository<User,Long> {
+    User findByEmail(@Param("email") String email);
+
+
+    List<User> findAll();
 }
