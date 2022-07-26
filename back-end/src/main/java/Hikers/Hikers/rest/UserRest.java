f1b11ad1a9cd29a77374a7a427d13c27117a87ad
CHATHURALIYANAGE1997 <chathurabimalka@gmail.com>
2022-07-26 20:34:24 +0530

    Update UserRest.java

--- back-end/src/main/java/Hikers/Hikers/rest/UserRest.java
+++ back-end/src/main/java/Hikers/Hikers/rest/UserRest.java
@@ -1,11 +1,22 @@
 package Hikers.Hikers.rest;
 
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Map;
 
 @CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping(path = "/user")
 public interface UserRest {
+    
+    @PostMapping(path="/signup")
+    public ResponseEntity<String> signUp(@RequestBody(required=true) Map<String, String> requestMap);
+
+    @PostMapping(path = "/login")
+    public ResponseEntity<String> login(@RequestBody(required = true) Map<String,String> requestMap);
 
 }
