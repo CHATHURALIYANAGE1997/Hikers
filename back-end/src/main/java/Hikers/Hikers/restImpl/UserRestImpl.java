cb4552f260d7eeaf1dbad40a57e1bef2f6090915
Chathura Liyanage <84259289+CHATHURALIYANAGE1997@users.noreply.github.com>
2022-07-27 09:12:18 +0530

    Update UserRestImpl.java

--- back-end/src/main/java/Hikers/Hikers/restImpl/UserRestImpl.java
+++ back-end/src/main/java/Hikers/Hikers/restImpl/UserRestImpl.java
@@ -1,14 +1,21 @@
 package Hikers.Hikers.restImpl;
 
+import Hikers.Hikers.cons.Hcons;
 import Hikers.Hikers.rest.UserRest;
+import Hikers.Hikers.service.UserService;
+import Hikers.Hikers.utils.Hutils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
-import Hikers.Hikers.service.UserService;
+
+import java.util.Map;
 
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController("/user")
 public class UserRestImpl implements UserRest {
-   @Autowired
+    @Autowired
     private UserService userService;
 
     @Override
@@ -22,4 +29,15 @@ public class UserRestImpl implements UserRest {
         return Hutils.getResponseEntity(Hcons.SOMETHIMG_WENT_WRONG, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
+    @Override
+    public ResponseEntity<String> login(Map<String, String> requestMap) {
+        try {
+
+            return userService.login(requestMap);
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+        return Hutils.getResponseEntity(Hcons.SOMETHIMG_WENT_WRONG,HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 }
+
