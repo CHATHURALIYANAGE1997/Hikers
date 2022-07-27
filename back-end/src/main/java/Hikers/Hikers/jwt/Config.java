77f9f65532a189dec70297324e149842144ea98e
Chathura Liyanage <84259289+CHATHURALIYANAGE1997@users.noreply.github.com>
2022-07-27 09:15:20 +0530

    Update Config.java

--- back-end/src/main/java/Hikers/Hikers/jwt/Config.java
+++ back-end/src/main/java/Hikers/Hikers/jwt/Config.java
@@ -1,4 +1,19 @@
 package Hikers.Hikers.jwt;
 
-public class Config {
+@Configuration
+@EnableWebSecurity
+public class Config extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    CustomerUserDetailsService customerUserDetailsService;
+
+    @Autowired
+    JwtFilter jwtFilter;
+
+
+    @Bean(name = BeanIds.AUTHENTICATION_MANAGER)
+    @Override
+    protected AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
+    }
 }
