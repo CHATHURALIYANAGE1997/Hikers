e858a11869a894ddd7b394708cbe4d9066cd1a9a
CHATHURALIYANAGE1997 <chathurabimalka@gmail.com>
2022-07-27 13:41:38 +0530

    Update index.js

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -6,14 +6,8 @@ import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "react-phone-number-input/style.css";
 import 'font-awesome/css/font-awesome.min.css';
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import {Provider} from 'react-redux';
+import store from './services/store';
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
