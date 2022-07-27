8b3a72287beccae7de07164635d97d6e0a698761
Chathura Liyanage <84259289+CHATHURALIYANAGE1997@users.noreply.github.com>
2022-07-27 09:09:28 +0530

    Update UserRestImpl.java

--- back-end/src/main/java/Hikers/Hikers/restImpl/UserRestImpl.java
+++ back-end/src/main/java/Hikers/Hikers/restImpl/UserRestImpl.java
@@ -3,9 +3,23 @@ package Hikers.Hikers.restImpl;
 import Hikers.Hikers.rest.UserRest;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
+import Hikers.Hikers.service.UserService;
 
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController("/user")
 public class UserRestImpl implements UserRest {
+   @Autowired
+    private UserService userService;
+
+    @Override
+    public ResponseEntity<String> signUp(Map<String, String> requestMap) {
+        try {
+
+            return userService.signUp(requestMap);
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+        return Hutils.getResponseEntity(Hcons.SOMETHIMG_WENT_WRONG, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 
 }
