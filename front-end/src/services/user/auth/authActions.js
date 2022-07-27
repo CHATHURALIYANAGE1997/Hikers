79f522e2cd9b5489c7ee4e68b6eb2c8d0d8b4b56
CHATHURALIYANAGE1997 <chathurabimalka@gmail.com>
2022-07-27 12:54:56 +0530

    Update authActions.js

--- front-end/src/services/user/auth/authActions.js
+++ front-end/src/services/user/auth/authActions.js
@@ -21,4 +21,37 @@ export const authenticateUser = (email, password) => async (dispatch) => {
         return Promise.reject(error);
     }
 };
+export const logoutUser = () => {
+    return (dispatch) => {
+        dispatch(logoutRequest());
+        localStorage.removeItem("jwtToken");
+        dispatch(success({ username: "", isLoggedIn: false }));
+    };
+};
+
+const loginRequest = () => {
+    return {
+        type: AT.LOGIN_REQUEST,
+    };
+};
+
+const logoutRequest = () => {
+    return {
+        type: AT.LOGOUT_REQUEST,
+    };
+};
+
+export const success = (isLoggedIn) => {
+    return {
+        type: AT.SUCCESS,
+        payload: isLoggedIn,
+    };
+};
+
+const failure = () => {
+    return {
+        type: AT.FAILURE,
+        payload: false,
+    };
+};
 
