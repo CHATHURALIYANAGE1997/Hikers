8a8544252e77ecf49b471cdccd435fb929190f15
Chathura Liyanage <84259289+CHATHURALIYANAGE1997@users.noreply.github.com>
2022-07-27 09:11:26 +0530

    Update UserServiceImpl.java

--- back-end/src/main/java/Hikers/Hikers/serviceImpl/UserServiceImpl.java
+++ back-end/src/main/java/Hikers/Hikers/serviceImpl/UserServiceImpl.java
@@ -8,4 +8,36 @@ import org.springframework.stereotype.Service;
 @Slf4j
 @Service
 public class UserServiceImpl implements UserService {
+  @Autowired
+    private UserRepo userRepo;
+
+    @Autowired
+    AuthenticationManager authenticationManager;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private CustomerUserDetailsService customerUserDetailsService;
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @Override
+    public ResponseEntity<String> signUp(Map<String, String> requestMap) {
+        log.info("Inside signup{}",requestMap);
+        try {
+            User user = userRepo.findByEmail(requestMap.get("email"));
+            if (Objects.isNull(user)) {
+                //user.setPassword(passwordEncoder.encode(user.getPassword()));
+                userRepo.save(getUserFromMap(requestMap));
+                return Hutils.getResponseEntity("SuccessFully Registrered", HttpStatus.OK);
+            } else {
+                return Hutils.getResponseEntity("Email is already used", HttpStatus.BAD_REQUEST);
+            }
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+        return Hutils.getResponseEntity(Hcons.SOMETHIMG_WENT_WRONG,HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 }
