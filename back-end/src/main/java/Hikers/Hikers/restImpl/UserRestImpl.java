80c1f055e56db21b5273b1af10e3fde1a7ed33ab
Chathura Liyanage <84259289+CHATHURALIYANAGE1997@users.noreply.github.com>
2022-07-27 20:49:48 +0530

    Update UserRestImpl.java

--- back-end/src/main/java/Hikers/Hikers/restImpl/UserRestImpl.java
+++ back-end/src/main/java/Hikers/Hikers/restImpl/UserRestImpl.java
@@ -28,24 +28,15 @@ public class UserRestImpl implements UserRest {
         }
         return Hutils.getResponseEntity(Hcons.SOMETHIMG_WENT_WRONG, HttpStatus.INTERNAL_SERVER_ERROR);
     }
-<<<<<<< Updated upstream
-
-    @Override
+ @Override
     public ResponseEntity<String> login(Map<String, String> requestMap) {
         try {
 
-=======
-
-    @Override
-    public ResponseEntity<String> login(Map<String, String> requestMap) {
-        try {
-
->>>>>>> Stashed changes
             return userService.login(requestMap);
         }catch (Exception ex){
             ex.printStackTrace();
         }
-        return Hutils.getResponseEntity(Hcons.SOMETHIMG_WENT_WRONG,HttpStatus.INTERNAL_SERVER_ERROR);
+        return CafeUntils.getResponseEntity(CafeConstents.SOMETHIMG_WENT_WRONG,HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
 
