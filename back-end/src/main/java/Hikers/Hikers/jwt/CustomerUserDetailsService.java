0458ebe60a47f2d18fefa2c7633e6dbbabe21e6b
Chathura Liyanage <84259289+CHATHURALIYANAGE1997@users.noreply.github.com>
2022-07-27 09:15:52 +0530

    Update CustomerUserDetailsService.java

--- back-end/src/main/java/Hikers/Hikers/jwt/CustomerUserDetailsService.java
+++ back-end/src/main/java/Hikers/Hikers/jwt/CustomerUserDetailsService.java
@@ -1,4 +1,41 @@
 package Hikers.Hikers.jwt;
 
-public class CustomerUserDetailsService {
+import Hikers.Hikers.model.User;
+import Hikers.Hikers.repository.UserRepo;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Objects;
+
+
+@Slf4j
+@Service
+public class CustomerUserDetailsService implements UserDetailsService {
+    @Autowired
+    private UserRepo userRepo;
+
+    private User userDetails;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        log.info("Inside loadUserByUsername {}",username);
+        userDetails=userRepo.findByEmail(username);
+        if(!Objects.isNull(userDetails)){
+            return new org.springframework.security.core.userdetails.User(userDetails.getEmail(),userDetails.getPassword(),new ArrayList<>());
+        }
+        else {
+            throw new UsernameNotFoundException("User Not Found");
+        }
+    }
+
+    public User getUserDetails(){
+        return userDetails;
+    }
 }
+
