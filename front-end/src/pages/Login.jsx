a8c1fda07520a8677c3e0da8cf92c83b574730cb
CHATHURALIYANAGE1997 <chathurabimalka@gmail.com>
2022-07-28 14:16:11 +0530

    Update Login.jsx

--- front-end/src/pages/Login.jsx
+++ front-end/src/pages/Login.jsx
@@ -62,7 +62,7 @@ const Login = (props) => {
 
                     <form class="row g-3">
                         <div className="col-12">
-                            {/* <br></br> */}
+
                             {show && props.message && (
                                 <Alert variant="success" onClose={() => setShow(false)} dismissible>
                                     {props.message}
@@ -109,54 +109,6 @@ const Login = (props) => {
 
             </div>
         </div >
-
-
-
-
-        // <div>
-        //     <div class="container-fluid ps-md-0">
-        //         <div class="row g-0">
-        //             <div class="d-none d-md-flex col-md-4 col-lg-6 bg-image"></div>
-        //             <div class="col-md-8 col-lg-6">
-        //                 <div class="login d-flex align-items-center py-5">
-        //                     <div class="container">
-        //                         <div class="row">
-        //                             <div class="col-md-9 col-lg-8 mx-auto">
-        //                                 <h3 class="login-heading mb-4">Welcome To Hikers!</h3>
-
-        //                                 <form>
-        //                                     <div class="form-floating mb-3">
-        //                                         <input type="email" class="form-control" value={user.email} onChange={credentialChange} placeholder="name@example.com" name="email" />
-        //                                         <label for="floatingInput" >Email address</label>
-        //                                     </div>
-        //                                     <div class="form-floating mb-3">
-        //                                         <input type="password" class="form-control" id="floatingPassword" value={user.password}  onChange={credentialChange} placeholder="Password" name="password"/>
-        //                                         <label for="floatingPassword" >Password</label>
-        //                                     </div>
-
-        //                                     <div class="form-check mb-3">
-        //                                         <input class="form-check-input" type="checkbox" value="" id="rememberPasswordCheck" />
-        //                                         <label class="form-check-label" for="rememberPasswordCheck">
-        //                                             Remember password
-        //                                         </label>
-        //                                     </div>
-        //                                 </form>
-        //                                     <div class="d-grid">
-        //                                         <button class="btn btn-lg btn-primary btn-login text-uppercase fw-bold mb-2" type="button" variant="success"  onClick={validateUser}>Sign in</button>
-
-        //                                         <div class="text-center">
-        //                                             <a class="small" >Forgot password?</a>
-        //                                         </div>
-        //                                     </div>
-
-        //                             </div>
-        //                         </div>
-        //                     </div>
-        //                 </div>
-        //             </div>
-        //         </div>
-        //     </div>
-        // </div>
     )
 }
 
