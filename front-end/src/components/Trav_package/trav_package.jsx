6526ccc281c8c5532d0b20a98cf3783ddfa68249
Shirantha Kellum <skellumjayasooriya@gmail.com>
2022-08-01 09:16:16 +0530

    Trav_package.

--- front-end/src/components/Trav_package/trav_package.jsx
+++ front-end/src/components/Trav_package/trav_package.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+import "./trav_package.css"
 
 // import { Component } from "react";
 
@@ -7,11 +9,34 @@ const Package = (props) => {
     console.log (props.name)
     return (
         <div className="package_1">
-            <h4>{props.name}</h4>
-            {/* <Form.Check reverse/>    */}
             <Form.Check reverse
                 label={props.name}
             />
+            <Row>
+                <div>
+                    <h5 className="txtLocal"><u>Local -</u></h5>
+
+                </div>
+            </Row>
+            <Row>
+                <div>
+                    <p className="localDes">Rs. {props.lPrice} per person.</p>
+
+                </div>
+            </Row>
+            <Row>
+                <div>
+                    <h5 className="txtForiegn"><u>Foriegn -</u></h5>
+
+                </div>
+            </Row>
+            <Row>
+                <div>
+                    <p className="ForiegnDes">Rs. {props.fPrice} per person.</p>
+
+                </div>
+            </Row>
+            
         </div>
     )
 }
--- front-end/src/pages/trav_selectPackages.jsx
+++ front-end/src/pages/trav_selectPackages.jsx
@@ -7,13 +7,13 @@ import Package from "../components/Trav_package/trav_package";
 const selectPackages = () => {
     return (
         <div>
-            {/* <div className="packageContainer"> */}
+            <div className="packageContainer mx-auto">
+                <h3>Select Yours</h3>
             <Stack gap={1} className="col-md-5 mx-auto">
-                <Package name="One Day Hike"/>
-                {/* <Button variant="outline-secondary">Cancel</Button>
-                <Button variant="outline-secondary">Cancel</Button> */}
+                <Package name="One Day Hike" lPrice="1000" fPrice="2000"/>
+                
             </Stack>
-            {/* </div> */}
+            </div>
         </div>
 
     )
