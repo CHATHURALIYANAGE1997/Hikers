d3c5770e55f5b34aa313a9bf677b6231ceaae2ae
Sathya Karunakalage <sathyaudayangi2@gmail.com>
2022-08-03 02:57:16 +0530

    Add Traveling Guide Profile Page

--- front-end/src/pages/TravelingGuide.jsx
+++ front-end/src/pages/TravelingGuide.jsx
@@ -1,19 +1,32 @@
 import React from "react";
 import GuideNav from "../components/Guide/GuideNav/GuideNav";
+import GuideReview from "../components/Guide/Profile/GuideReview";
+import ProfileCard from "../components/Guide/Profile/ProfileCard";
+import ProfileDetails from "../components/Guide/Profile/ProfileDetails";
 import Navbar from "../components/Navbar/Navbar";
 
 
 const TravelingGuide = () => {
     return (
-        <div className="d-flex flex-column">
+        <div className="d-flex flex-column tg-container">
             <Navbar />
-            <GuideNav />
+            <div className="d-flex flex-row tg-page">
+                <GuideNav />
+                <div className="d-flex flex-column tg-profile">
+                    <div className="d-flex flex-row ">
+                        <ProfileCard />
+                        <ProfileDetails />
+                    </div>
+                    <div className="d-flex flex-row tg-reviews">
+                        <GuideReview />
+                    </div>
+                    
+                </div>
+            </div>
+
         </div>
     );
 }
 
 export default TravelingGuide;
 
-//Install material ui
-//npm install @material-ui/core
-//npm install @material-ui/icons
