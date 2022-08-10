8a9ee427fbc4529b5045b026796fb8aa6a505e63
Subhagya <subhasankalpani@gmail.com>
2022-08-10 18:13:21 +0530

    changes done to the css file in camping equipment

--- front-end/src/pages/CampingEquipment.jsx
+++ front-end/src/pages/CampingEquipment.jsx
@@ -1,31 +1,27 @@
 import React from "react";
 import Navbar from "../components/Navbar/Navbar";
-import campingequipment from "../components/CampingEquipment/campingequipment.css";
-import Sidebar from "../components/CampingEquipment/Sidebar";
+import Sidebar from "../components/CampingEquipment/CampingNav/Sidebar";
+import Wall from "../components/CampingEquipment/Wall/Wall";
 import { useTitle } from "../components/Title/Title";
-// import background from "../components/images/background.jpg";
-
-
 
 
 const CampingEquipment = () => {
     useTitle("Hikers")
 
     return (
-        <div className="d-flex flex-column campingContainer">
-            <Navbar/>
-         <div className="d-flex flex-row campingContainersub">
-            
-            
-                <Sidebar/>
-            
-            
-        </div>   
-            
-            
-                             
-       </div>
-           
+     
+            <div className="d-flex flex-column campingContainer">
+                <Navbar/>
+                <div className="d-flex flex-row campingContainersub">
+                    <div>
+                    <Sidebar/>
+                    </div>
+                    <div class="d-flex flex-column campingWall">   
+                    <Wall/>
+                    </div>  
+                </div>                       
+            </div>
+       
        
     
         
