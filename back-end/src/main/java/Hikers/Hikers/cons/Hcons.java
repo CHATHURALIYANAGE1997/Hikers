f224c4e06c19c245b0de38ba24ce01849c733fa8
Chathura Liyanage <84259289+CHATHURALIYANAGE1997@users.noreply.github.com>
2022-08-15 14:59:41 +0530

    Update Hcons.java

--- back-end/src/main/java/Hikers/Hikers/cons/Hcons.java
+++ back-end/src/main/java/Hikers/Hikers/cons/Hcons.java
@@ -2,5 +2,7 @@ package Hikers.Hikers.cons;
 
 public class Hcons {
     public static final String SOMETHIMG_WENT_WRONG="Something Went Wrong";
+     public  static  final  String ACCESS_DINAED="Acces Dinaed";
+    
 
 }
