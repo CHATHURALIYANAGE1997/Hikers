efe25839c4c19aedced9041217e49dbf3a19e142
Chathura Liyanage <84259289+CHATHURALIYANAGE1997@users.noreply.github.com>
2022-08-15 16:55:05 +0530

    Update EquipmentproviderRepo.java

--- back-end/src/main/java/Hikers/Hikers/repository/EquipmentproviderRepo.java
+++ back-end/src/main/java/Hikers/Hikers/repository/EquipmentproviderRepo.java
@@ -1,1 +1,12 @@
+package Hikers.Hikers.repository;
 
+import Hikers.Hikers.model.Equipmentprovider;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EquipmentproviderRepo extends JpaRepository<Equipmentprovider,Long> {
+
+   
+}
