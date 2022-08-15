29e910b63209eac5871edc076c98aebdd69678d0
Chathura Liyanage <84259289+CHATHURALIYANAGE1997@users.noreply.github.com>
2022-08-15 16:59:57 +0530

    Update TravelingguideRepo.java

--- back-end/src/main/java/Hikers/Hikers/repository/TravelingguideRepo.java
+++ back-end/src/main/java/Hikers/Hikers/repository/TravelingguideRepo.java
@@ -1,12 +1,18 @@
 package Hikers.Hikers.repository;
 
 import Hikers.Hikers.model.Travelingguide;
+import Hikers.Hikers.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface TravelingguideRepo extends JpaRepository<Travelingguide,Long> {
-   
+   Travelingguide  findByEmail(@Param("email") String email);
+
+   Travelingguide findByContactNumber(@Param("contactNumber") String contactNumber);
+
+   List<Travelingguide> findAll();
 }
