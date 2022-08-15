b0f62ccffea96db59723c8ab09e278f9b59d3e9a
Chathura Liyanage <84259289+CHATHURALIYANAGE1997@users.noreply.github.com>
2022-08-15 15:00:17 +0530

    Update Equipmentprovider.java

--- back-end/src/main/java/Hikers/Hikers/model/Equipmentprovider.java
+++ back-end/src/main/java/Hikers/Hikers/model/Equipmentprovider.java
@@ -1,7 +1,43 @@
 package Hikers.Hikers.model;
 
 
+import lombok.Data;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Data
+@Entity
+@DynamicUpdate
+@DynamicInsert
+@Table(name = "equipmentprovider")
 public class Equipmentprovider implements Serializable {
-  
-  
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="provider_id")
+    private Long provider_id;
+
+    @Column(name="fullname")
+    private String fullname;
+
+    @Column(name="email")
+    private String email;
+
+    @Column(name="password")
+    private String password;
+
+    @Column(name="contactNumber")
+    private String contactNumber;
+
+    @Column(name="address")
+    private String address;
+
+    @Column(name="accountstatus")
+    private String accountstatus;
+
+    @Column(name="role")
+    private String role;
 }
