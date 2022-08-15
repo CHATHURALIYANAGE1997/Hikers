d0099ce641144856794a774aaf16807d14a6642e
Chathura Liyanage <84259289+CHATHURALIYANAGE1997@users.noreply.github.com>
2022-08-15 16:19:07 +0530

    Update UserRepo.java

--- back-end/src/main/java/Hikers/Hikers/repository/UserRepo.java
+++ back-end/src/main/java/Hikers/Hikers/repository/UserRepo.java
@@ -6,11 +6,19 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface UserRepo  extends JpaRepository<User,Long> {
+
     User findByEmail(@Param("email") String email);
 
+    User findByContactNumber(@Param("contactNumber") String contactNumber);
 
     List<User> findAll();
+
+    List<User> findByRole(@Param("role") String role);
+
+    Optional<User> findById(@Param("id") Long id);
 }
+
