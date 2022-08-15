07788fd21f45e8d8979592fff6ec16b0b1faff84
Chathura Liyanage <84259289+CHATHURALIYANAGE1997@users.noreply.github.com>
2022-08-15 16:17:20 +0530

    Update Travelingguide.java

--- back-end/src/main/java/Hikers/Hikers/model/Travelingguide.java
+++ back-end/src/main/java/Hikers/Hikers/model/Travelingguide.java
@@ -1,6 +1,71 @@
 package Hikers.Hikers.model;
 
+import lombok.Data;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
 
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Data
+@Entity
+@DynamicUpdate
+@DynamicInsert
+@Table(name = "travelingguide")
 public class Travelingguide implements Serializable {
- 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="guide_id")
+    private Long guide_id;
+
+    @Column(name="firstname")
+    private String firstname;
+
+    @Column(name="lastname")
+    private String lastname;
+
+    @Column(name="address")
+    private String address;
+
+    @Column(name="dob")
+    private String dob;
+
+    @Column(name="education_level")
+    private String education_level;
+
+    @Column(name="accountstatus")
+    private String accountstatus;
+
+    @Column(name="prolice_report")
+    private String prolice_report;
+
+    @Column(name="gender")
+    private String gender;
+
+    @Column(name="experience")
+    private String experience;
+
+    @Column(name="nic")
+    private String nic;
+
+    @Column(name="nic_image")
+    private String nic_image;
+
+    @Column(name="rate")
+    private String rate;
+
+    @Column(name="password")
+    private String password;
+
+    @Column(name="email")
+    private String email;
+
+    @Column(name="role")
+    private String role;
+
+    @Column(name="contactNumber")
+    private String contactNumber;
+
+
 }
+
