51add1d6197e7bae575e408383559db9a35660ad
Chathura Liyanage <84259289+CHATHURALIYANAGE1997@users.noreply.github.com>
2022-08-15 16:59:28 +0530

    Update TransportproviderRepo.java

--- back-end/src/main/java/Hikers/Hikers/repository/TransportproviderRepo.java
+++ back-end/src/main/java/Hikers/Hikers/repository/TransportproviderRepo.java
@@ -3,10 +3,12 @@ package Hikers.Hikers.repository;
 
 import Hikers.Hikers.model.Transportprovider;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TransportproviderRepo extends JpaRepository<Transportprovider,Long> {
-    
+    Transportprovider findByEmail(@Param("email") String email);
+
+    Transportprovider findByContactNumber(@Param("contactNumber") String contactNumber);
 }
