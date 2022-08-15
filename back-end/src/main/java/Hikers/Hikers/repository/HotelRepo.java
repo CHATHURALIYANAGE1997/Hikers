0cbf9794efffc00b25e58def9cf1dabebcb3ba8d
Chathura Liyanage <84259289+CHATHURALIYANAGE1997@users.noreply.github.com>
2022-08-15 16:59:00 +0530

    Update HotelRepo.java

--- back-end/src/main/java/Hikers/Hikers/repository/HotelRepo.java
+++ back-end/src/main/java/Hikers/Hikers/repository/HotelRepo.java
@@ -2,9 +2,13 @@ package Hikers.Hikers.repository;
 
 import Hikers.Hikers.model.Hotel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface HotelRepo extends JpaRepository<Hotel,Long> {
-   
+    Hotel findByEmail(@Param("email") String email);
+
+    Hotel findByContactNumber(@Param("contactNumber") String contactNumber);
 }
+
