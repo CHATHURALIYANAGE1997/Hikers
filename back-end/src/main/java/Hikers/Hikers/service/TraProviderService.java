bc9d2ce0c27c9846421b6efa952b0dfa44d769c2
Chathura Liyanage <84259289+CHATHURALIYANAGE1997@users.noreply.github.com>
2022-08-16 08:53:21 +0530

    Update TraProviderService.java

--- back-end/src/main/java/Hikers/Hikers/service/TraProviderService.java
+++ back-end/src/main/java/Hikers/Hikers/service/TraProviderService.java
@@ -1,1 +1,12 @@
+package Hikers.Hikers.service;
 
+
+import Hikers.Hikers.model.Transportprovider;
+import org.springframework.http.ResponseEntity;
+import java.util.List;
+
+public interface TraProviderService {
+    ResponseEntity<List<Transportprovider>> getTraproviders();
+
+    ResponseEntity<List<Transportprovider>> getTraprovider(Long service_provider_id);
+}
