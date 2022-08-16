06f923d11c522a4baee5d650fd8734e23d448915
Chathura Liyanage <84259289+CHATHURALIYANAGE1997@users.noreply.github.com>
2022-08-16 08:34:13 +0530

    Update AdminRestImpl.java

--- back-end/src/main/java/Hikers/Hikers/restImpl/AdminRestImpl.java
+++ back-end/src/main/java/Hikers/Hikers/restImpl/AdminRestImpl.java
@@ -4,5 +4,28 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController("/admin")
 public class AdminRestImpl implements AdminRest {
-  
+  @Autowired
+    private UserService userService;
+
+    @Autowired
+    private HotelService hotelService;
+
+    @Autowired
+    private TravelGuideService travelGuideService;
+
+    @Autowired
+    private TraProviderService traProviderService;
+
+    @Autowired
+    private JwtFilter jwtFilter;
+
+    @Override
+    public ResponseEntity<List<User>> getAllUsers() {
+        try {
+            return userService.getAllUsers();
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+        return new ResponseEntity<List<User>>(new ArrayList<>(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 }
