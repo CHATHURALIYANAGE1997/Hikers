b6bd761eea66f34bdd9e8125a51cd04c5212dbe0
Chathura Liyanage <84259289+CHATHURALIYANAGE1997@users.noreply.github.com>
2022-08-16 08:54:45 +0530

    Update HotelServiceImpl.java

--- back-end/src/main/java/Hikers/Hikers/serviceImpl/HotelServiceImpl.java
+++ back-end/src/main/java/Hikers/Hikers/serviceImpl/HotelServiceImpl.java
@@ -1,1 +1,51 @@
+package Hikers.Hikers.serviceImpl;
 
+import Hikers.Hikers.jwt.JwtFilter;
+import Hikers.Hikers.model.Hotel;
+import Hikers.Hikers.repository.HotelRepo;
+import Hikers.Hikers.service.HotelService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Slf4j
+@Service
+public class HotelServiceImpl implements HotelService {
+
+    @Autowired
+    private HotelRepo hotelRepo;
+
+    @Autowired
+    private JwtFilter jwtFilter;
+
+    @Override
+    public ResponseEntity<List<Hotel>> getHotels() {
+        try {
+                return new ResponseEntity<>(hotelRepo.findAll(), HttpStatus.OK);
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+        return new ResponseEntity<>(new ArrayList<>(),HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @Override
+    public ResponseEntity<Optional<Hotel>> getHotel(Long hotel_id) {
+       try {
+           Optional<Hotel> hotel=hotelRepo.findById(hotel_id);
+           if(hotel.isPresent()){
+               return new ResponseEntity(hotel, HttpStatus.OK);
+           }else{
+               return new ResponseEntity(new ArrayList<>(), HttpStatus.BAD_REQUEST);
+           }
+       }catch (Exception ex){
+           ex.printStackTrace();
+       }
+        return new ResponseEntity(new ArrayList<>(),HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
