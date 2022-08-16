0896af6773679295ee4a9d6ff1fbd5782555718f
Chathura Liyanage <84259289+CHATHURALIYANAGE1997@users.noreply.github.com>
2022-08-16 08:52:43 +0530

    Update HotelService.java

--- back-end/src/main/java/Hikers/Hikers/service/HotelService.java
+++ back-end/src/main/java/Hikers/Hikers/service/HotelService.java
@@ -1,1 +1,13 @@
+package Hikers.Hikers.service;
 
+import Hikers.Hikers.model.Hotel;
+import org.springframework.http.ResponseEntity;
+import java.util.List;
+import java.util.Optional;
+
+public interface HotelService {
+    ResponseEntity<List<Hotel>> getHotels();
+
+    ResponseEntity<Optional<Hotel>> getHotel(Long hotel_id);
+
+}
