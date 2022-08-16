59a54a7c9c4c8b8bc2db969acd0f1a80dee247fe
Chathura Liyanage <84259289+CHATHURALIYANAGE1997@users.noreply.github.com>
2022-08-16 08:32:04 +0530

    Update AdminRestImpl.java

--- back-end/src/main/java/Hikers/Hikers/restImpl/AdminRestImpl.java
+++ back-end/src/main/java/Hikers/Hikers/restImpl/AdminRestImpl.java
@@ -1,1 +1,8 @@
+package Hikers.Hikers.restImpl;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/admin")
+public class AdminRestImpl implements AdminRest {
+  
+}
