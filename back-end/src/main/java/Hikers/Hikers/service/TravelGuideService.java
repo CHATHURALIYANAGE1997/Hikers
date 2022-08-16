152b53147a008dac82a5301db487a2098942fb96
Chathura Liyanage <84259289+CHATHURALIYANAGE1997@users.noreply.github.com>
2022-08-16 08:53:56 +0530

    Update TravelGuideService.java

--- back-end/src/main/java/Hikers/Hikers/service/TravelGuideService.java
+++ back-end/src/main/java/Hikers/Hikers/service/TravelGuideService.java
@@ -1,1 +1,12 @@
+package Hikers.Hikers.service;
 
+import Hikers.Hikers.model.Travelingguide;
+import org.springframework.http.ResponseEntity;
+import java.util.List;
+import java.util.Optional;
+
+public interface TravelGuideService {
+    ResponseEntity<List<Travelingguide>> getGuides();
+
+    ResponseEntity<Optional<Travelingguide>> getGuide(Long guide_id);
+}
