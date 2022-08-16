26bcc7914b9db323a5add1caa92fd30e5511cd5e
Chathura Liyanage <84259289+CHATHURALIYANAGE1997@users.noreply.github.com>
2022-08-16 08:34:56 +0530

    Update UserRestImpl.java

--- back-end/src/main/java/Hikers/Hikers/restImpl/UserRestImpl.java
+++ back-end/src/main/java/Hikers/Hikers/restImpl/UserRestImpl.java
@@ -1,22 +1,37 @@
 package Hikers.Hikers.restImpl;
 
 import Hikers.Hikers.cons.Hcons;
+import Hikers.Hikers.jwt.JwtFilter;
+import Hikers.Hikers.model.Hotel;
+import Hikers.Hikers.model.Transportprovider;
 import Hikers.Hikers.rest.UserRest;
+import Hikers.Hikers.service.HotelService;
+import Hikers.Hikers.service.TraProviderService;
 import Hikers.Hikers.service.UserService;
 import Hikers.Hikers.utils.Hutils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
-
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
-@CrossOrigin(origins = "http://localhost:3000")
+
 @RestController("/user")
 public class UserRestImpl implements UserRest {
     @Autowired
     private UserService userService;
+
+    @Autowired
+    HotelService hotelService;
+
+    @Autowired
+    private JwtFilter jwtFilter;
+
+    @Autowired
+    private TraProviderService traProviderService;
 
     @Override
     public ResponseEntity<String> signUp(Map<String, String> requestMap) {
@@ -28,7 +43,7 @@ public class UserRestImpl implements UserRest {
         }
         return Hutils.getResponseEntity(Hcons.SOMETHIMG_WENT_WRONG, HttpStatus.INTERNAL_SERVER_ERROR);
     }
-    
+
     @Override
     public ResponseEntity<String> signUphotel(Map<String, String> requestMap) {
         try {
@@ -48,7 +63,7 @@ public class UserRestImpl implements UserRest {
         }
         return Hutils.getResponseEntity(Hcons.SOMETHIMG_WENT_WRONG, HttpStatus.INTERNAL_SERVER_ERROR);
     }
-    
+
     @Override
     public ResponseEntity<String> signUpequipmentprovider(Map<String, String> requestMap) {
         try {
@@ -68,7 +83,8 @@ public class UserRestImpl implements UserRest {
         }
         return Hutils.getResponseEntity(Hcons.SOMETHIMG_WENT_WRONG, HttpStatus.INTERNAL_SERVER_ERROR);
     }
-    
+
+
     @Override
     public ResponseEntity<String> login(Map<String, String> requestMap) {
         try {
@@ -79,4 +95,59 @@ public class UserRestImpl implements UserRest {
         }
         return Hutils.getResponseEntity(Hcons.SOMETHIMG_WENT_WRONG,HttpStatus.INTERNAL_SERVER_ERROR);
     }
+
+    @Override
+    public ResponseEntity<String> addCoAdmin(Map<String, String> requestMap) {
+        try {
+            if(jwtFilter.isAdmin() || jwtFilter.isCoAdmin()) {
+                return userService.signUp(requestMap);
+            }else {
+               return Hutils.getResponseEntity(Hcons.ACCESS_DINAED,HttpStatus.UNAUTHORIZED);
+           }
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+        return Hutils.getResponseEntity(Hcons.SOMETHIMG_WENT_WRONG,HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @Override
+    public ResponseEntity<?> userprofile() {
+        try {
+            return userService.userProfile();
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+        return new ResponseEntity<>(new ArrayList<>(),HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @Override
+    public ResponseEntity<List<Hotel>> gethotels() {
+        try{
+            return hotelService.getHotels();
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+        return new ResponseEntity<>(new ArrayList<>(),HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @Override
+    public ResponseEntity<Optional<Hotel>> getHotel(Long hotel_id) {
+        try{
+            return hotelService.getHotel(hotel_id);
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+        return new ResponseEntity(new ArrayList<>(), HttpStatus.BAD_REQUEST);
+    }
+
+    @Override
+    public ResponseEntity<Optional<Transportprovider>> getTraProvide(Long service_provider_id) {
+        try{
+            return userService.getTraProvide(service_provider_id);
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+        return new ResponseEntity(new ArrayList<>(), HttpStatus.BAD_REQUEST);
+    }
+
 }
