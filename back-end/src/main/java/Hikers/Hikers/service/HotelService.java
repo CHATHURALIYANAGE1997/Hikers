d5fcc3850dfc88f112968d1a7896723dbab3bb8e
Chathura Liyanage <84259289+CHATHURALIYANAGE1997@users.noreply.github.com>
2022-08-31 23:53:30 +0530

    Update HotelService.java

--- back-end/src/main/java/Hikers/Hikers/service/HotelService.java
+++ back-end/src/main/java/Hikers/Hikers/service/HotelService.java
@@ -3,6 +3,7 @@ package Hikers.Hikers.service;
 import Hikers.Hikers.model.Hotel;
 import org.springframework.http.ResponseEntity;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 public interface HotelService {
@@ -10,4 +11,5 @@ public interface HotelService {
 
     ResponseEntity<Optional<Hotel>> getHotel(Long hotel_id);
 
+    ResponseEntity<String> changePassoword(String id, Map<String, String> requestMap);
 }
