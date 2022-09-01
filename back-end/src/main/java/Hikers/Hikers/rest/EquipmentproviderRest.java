0cdede441537e2404643a89b70b458da49c1ac35
Chathura Liyanage <84259289+CHATHURALIYANAGE1997@users.noreply.github.com>
2022-09-01 12:58:51 +0530

    Update EquipmentproviderRest.java

--- back-end/src/main/java/Hikers/Hikers/rest/EquipmentproviderRest.java
+++ back-end/src/main/java/Hikers/Hikers/rest/EquipmentproviderRest.java
@@ -1,5 +1,13 @@
 package Hikers.Hikers.rest;
 
-public interface EquipmentproviderRest {
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Map;
+
+@CrossOrigin(origins = "http://localhost:3000")
+@RequestMapping(path = "/equprovider")
+public interface EquipmentproviderRest {
+    @PostMapping(path="/changepassword/{id}")
+    public ResponseEntity<String> changePassoword(@PathVariable("id") String id, @RequestBody(required=true) Map<String, String> requestMap);
 }
