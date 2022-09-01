9f6d154c988081af964ce5e62062689d636a42d1
Chathura Liyanage <84259289+CHATHURALIYANAGE1997@users.noreply.github.com>
2022-09-01 14:18:41 +0530

    Update EquipmentproviderRestImpl.java

--- back-end/src/main/java/Hikers/Hikers/restImpl/EquipmentproviderRestImpl.java
+++ back-end/src/main/java/Hikers/Hikers/restImpl/EquipmentproviderRestImpl.java
@@ -1,6 +1,11 @@
 package Hikers.Hikers.restImpl;
 
+import Hikers.Hikers.cons.Hcons;
 import Hikers.Hikers.rest.EquipmentproviderRest;
+import Hikers.Hikers.service.EquipmentproviderService;
+import Hikers.Hikers.utils.Hutils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,8 +14,17 @@ import java.util.Map;
 @RestController("/equprovider")
 public class EquipmentproviderRestImpl implements EquipmentproviderRest {
 
+    @Autowired
+    private EquipmentproviderService equipmentproviderService;
+
     @Override
     public ResponseEntity<String> changePassoword(String id, Map<String, String> requestMap) {
-        return null;
+        try {
+            return equipmentproviderService.changePassoword(id,requestMap);
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+        return Hutils.getResponseEntity(Hcons.SOMETHIMG_WENT_WRONG, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
+
