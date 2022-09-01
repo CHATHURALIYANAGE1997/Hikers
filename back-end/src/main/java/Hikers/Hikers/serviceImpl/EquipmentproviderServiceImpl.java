6d99681037de3c0765d486304f17cb2a1b8bd064
Chathura Liyanage <84259289+CHATHURALIYANAGE1997@users.noreply.github.com>
2022-09-01 14:20:07 +0530

    Update EquipmentproviderServiceImpl.java

--- back-end/src/main/java/Hikers/Hikers/serviceImpl/EquipmentproviderServiceImpl.java
+++ back-end/src/main/java/Hikers/Hikers/serviceImpl/EquipmentproviderServiceImpl.java
@@ -1,7 +1,24 @@
 package Hikers.Hikers.serviceImpl;
 
 import Hikers.Hikers.service.EquipmentproviderService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Map;
+@Slf4j
+@Service
 public class EquipmentproviderServiceImpl implements EquipmentproviderService {
-  
+
+    @Override
+    public ResponseEntity<String> changePassoword(String id, Map<String, String> requestMap) {
+        try {
+            return null;
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+        return new ResponseEntity(new ArrayList<>(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 }
