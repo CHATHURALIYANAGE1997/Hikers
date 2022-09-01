e60e5c153f0a4a2756737e95a0bc9f50dba83cf5
Chathura Liyanage <84259289+CHATHURALIYANAGE1997@users.noreply.github.com>
2022-09-01 13:03:07 +0530

    Update EquipmentproviderRestImpl.java

--- back-end/src/main/java/Hikers/Hikers/restImpl/EquipmentproviderRestImpl.java
+++ back-end/src/main/java/Hikers/Hikers/restImpl/EquipmentproviderRestImpl.java
@@ -1,5 +1,16 @@
 package Hikers.Hikers.restImpl;
 
+import Hikers.Hikers.rest.EquipmentproviderRest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController("/equprovider")
 public class EquipmentproviderRestImpl implements EquipmentproviderRest {
 
+    @Override
+    public ResponseEntity<String> changePassoword(String id, Map<String, String> requestMap) {
+        return null;
+    }
 }
