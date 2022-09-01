79358ade73accb2ce9ccf7f6b337c9ef88007a3e
Chathura Liyanage <84259289+CHATHURALIYANAGE1997@users.noreply.github.com>
2022-09-01 18:15:06 +0530

    Update CustomUserDetails.java

--- back-end/src/main/java/Hikers/Hikers/serviceImpl/CustomUserDetails.java
+++ back-end/src/main/java/Hikers/Hikers/serviceImpl/CustomUserDetails.java
@@ -60,34 +60,6 @@ public class CustomUserDetails implements UserDetails {
         return false;
     }
 
-//    private void sendVerificationEmail(User user, String siteURL)
-//            throws MessagingException, UnsupportedEncodingException {
-//        String toAddress = user.getEmail();
-//        String fromAddress = "Your email address";
-//        String senderName = "Your company name";
-//        String subject = "Please verify your registration";
-//        String content = "Dear [[name]],<br>"
-//                + "Please click the link below to verify your registration:<br>"
-//                + "<h3><a href=\"[[URL]]\" target=\"_self\">VERIFY</a></h3>"
-//                + "Thank you,<br>"
-//                + "Your company name.";
-//
-//        MimeMessage message = mailSender.createMimeMessage();
-//        MimeMessageHelper helper = new MimeMessageHelper(message);
-//
-//        helper.setFrom(fromAddress, senderName);
-//        helper.setTo(toAddress);
-//        helper.setSubject(subject);
-//
-//        content = content.replace("[[name]]", user.getFirstname());
-//        String verifyURL = siteURL + "/verify?code=" + user.getVerificationCode();
-//
-//        content = content.replace("[[URL]]", verifyURL);
-//
-//        helper.setText(content, true);
-//
-//        mailSender.send(message);
-//
-//    }
+
 
 }
