b6e1f47a2db0776f6d4af069755c2281a1dbee27
Chathura Liyanage <84259289+CHATHURALIYANAGE1997@users.noreply.github.com>
2022-09-01 14:16:06 +0530

    Update EquipmentproviderService.java

--- back-end/src/main/java/Hikers/Hikers/service/EquipmentproviderService.java
+++ back-end/src/main/java/Hikers/Hikers/service/EquipmentproviderService.java
@@ -1,4 +1,9 @@
 package Hikers.Hikers.service;
 
+import org.springframework.http.ResponseEntity;
+
+import java.util.Map;
+
 public interface EquipmentproviderService {
+    ResponseEntity<String> changePassoword(String id, Map<String, String> requestMap);
 }
