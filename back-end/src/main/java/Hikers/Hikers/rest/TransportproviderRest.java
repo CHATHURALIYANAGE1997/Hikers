7cc4dcf3a6ad85da950babfb1f9dd5b5677ec8a3
Chathura Liyanage <84259289+CHATHURALIYANAGE1997@users.noreply.github.com>
2022-09-02 21:46:09 +0530

    Create TransportproviderRest.java

---
+++ back-end/src/main/java/Hikers/Hikers/rest/TransportproviderRest.java
@@ -0,0 +1,11 @@
+package Hikers.Hikers.rest;
+
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@CrossOrigin(origins = "http://localhost:3000")
+@RequestMapping(path = "/Transport")
+public interface TransportproviderRest {
+
+}
