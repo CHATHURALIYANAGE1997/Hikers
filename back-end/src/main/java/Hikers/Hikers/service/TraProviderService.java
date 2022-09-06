2677d16d86892295d6e972293fceccb0aaed6605
Chathura Liyanage <84259289+CHATHURALIYANAGE1997@users.noreply.github.com>
2022-09-07 00:51:15 +0530

    Update TraProviderService.java

--- back-end/src/main/java/Hikers/Hikers/service/TraProviderService.java
+++ back-end/src/main/java/Hikers/Hikers/service/TraProviderService.java
@@ -4,6 +4,7 @@ package Hikers.Hikers.service;
 import Hikers.Hikers.model.Transportprovider;
 import org.springframework.http.ResponseEntity;
 import java.util.List;
+import java.util.Map;
 
 public interface TraProviderService {
     ResponseEntity<List<Transportprovider>> getTraproviders();
@@ -11,5 +12,5 @@ public interface TraProviderService {
     ResponseEntity<List<Transportprovider>> getTraprovider(Long service_provider_id);
     
     ResponseEntity<String> changePassoword(String id, Map<String, String> requestMap);
-
+    
 }
