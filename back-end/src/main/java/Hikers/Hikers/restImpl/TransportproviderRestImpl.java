f94b40faf010d62e6085c6a645f777b115ea282a
Chathura Liyanage <84259289+CHATHURALIYANAGE1997@users.noreply.github.com>
2022-09-07 00:48:56 +0530

    Update TransportproviderRestImpl.java

--- back-end/src/main/java/Hikers/Hikers/restImpl/TransportproviderRestImpl.java
+++ back-end/src/main/java/Hikers/Hikers/restImpl/TransportproviderRestImpl.java
@@ -1,7 +1,10 @@
 package Hikers.Hikers.restImpl;
 
 import Hikers.Hikers.rest.TransportproviderRest;
+import org.springframework.web.bind.annotation.RestController;
 
+
+@RestController
 public class TransportproviderRestImpl implements TransportproviderRest {
     
 }
