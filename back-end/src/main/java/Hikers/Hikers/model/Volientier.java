f6dc070cee732764724ec7afc2274606991f2008
Chathura Liyanage <84259289+CHATHURALIYANAGE1997@users.noreply.github.com>
2022-10-23 22:07:20 +0530

    Add files via upload

---
+++ back-end/src/main/java/Hikers/Hikers/model/Volientier.java
@@ -0,0 +1,29 @@
+package Hikers.Hikers.model;
+
+
+import lombok.Data;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Data
+@Entity
+@DynamicUpdate
+@DynamicInsert
+@Table(name = "volientier")
+public class Volientier implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+    private  String  contactnumber;
+    
+}
