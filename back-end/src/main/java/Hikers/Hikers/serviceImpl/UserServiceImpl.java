42c1d39c54433f79431fe850beeaf5074ec9eb51
Chathura Liyanage <84259289+CHATHURALIYANAGE1997@users.noreply.github.com>
2022-10-24 00:01:47 +0530

    Update UserServiceImpl.java

--- back-end/src/main/java/Hikers/Hikers/serviceImpl/UserServiceImpl.java
+++ back-end/src/main/java/Hikers/Hikers/serviceImpl/UserServiceImpl.java
@@ -388,7 +388,7 @@ public class UserServiceImpl implements UserService {
    public ResponseEntity<?> verifyGuideByAdmin(String code) {
         try {
             Travelingguide travelingguide=travelingguideRepo.findByEmail(code);
-            travelingguide.setAccountstatus("true");
+            travelingguide.setAccountstatus("ture");
             travelingguideRepo.save(travelingguide);
             return Hutils.getResponseEntity("Account status update successfully", HttpStatus.OK);
         }catch (Exception ex){
