9e21d64a87aa855865237cd8fd75adbe8dab4340
sathyaudayangi <sathyaudayangi2@gmail.com>
2022-10-23 18:37:23 +0530

    Display Guide Profile Data

--- front-end/src/components/Guide/Profile/ProfileCard.jsx
+++ front-end/src/components/Guide/Profile/ProfileCard.jsx
@@ -1,14 +1,21 @@
 import React from "react";
 import './profile.css';
 import profile from './Picture.png';
+import axios from "axios";
+import { useState } from "react";
+import { useEffect } from "react";
+import authToken from "../../../utils/authToken";
+import { useSelector } from "react-redux";
 
-const ProfileCard = () => {
+const ProfileCard = (props) => {
+
+    const {guide} = props;
+
     return (
         <div class="pc-container">
-            <div className="pc-card">
-                
+            <div className="pc-card">             
                     <img src={profile} className="pc-pic" alt="Profile Picture"></img>
-                    <h4 className="pc-name">M.S.D Perera</h4>
+                    <h4 className="pc-name">{guide.firstname} {" "}{guide.lastname}</h4>
                     <h7 className="pc-name">Traveling Guide</h7>
              
             </div>
--- front-end/src/components/Guide/Profile/ProfileDetails.jsx
+++ front-end/src/components/Guide/Profile/ProfileDetails.jsx
@@ -2,40 +2,60 @@ import React from "react";
 import './profile.css';
 import profile from './Picture.png';
 
-const ProfileDetails = () => {
-    return (
-        <div class="pd-container">
-            <div className="pd-card">
-                <div className="d-flex flex-column pd-row">
-                    <div className="d-flex flex-row">
-                        <div className="h6 pd-h6">Full Name</div>
-                        <div className="h6 pd-details">: MSD Perera</div>
+const ProfileDetails = (props) => {
+
+    
+
+    const displayGuide = (props) => {
+
+        const {guide} = props;
+
+        return (
+            <div class="pd-container">
+                <div className="pd-card">
+                    <div className="d-flex flex-column pd-row">
+                        <div className="d-flex flex-row">
+                            <div className="h6 pd-h6">Full Name</div>
+                            <div className="h6 pd-details">: {guide.firstname} {" "}{guide.lastname}</div>
+                        </div>
+                        <div className="d-flex flex-row">
+                            <div className="h6 pd-h6">E-mail</div>
+                            <div className="h6 pd-details">: {guide.email} </div>
+                        </div>
+                        <div className="d-flex flex-row">
+                            <div className="h6 pd-h6">NIC</div>
+                            <div className="h6 pd-details">: {guide.nic} </div>
+                        </div>
+                        <div className="d-flex flex-row">
+                            <div className="h6 pd-h6">Date of Birth</div>
+                            <div className="h6 pd-details">: {guide.dob} </div>
+                        </div>
+                        <div className="d-flex flex-row">
+                            <div className="h6 pd-h6">Gender</div>
+                            <div className="h6 pd-details">: {guide.gender} </div>
+                        </div>
+                        <div className="d-flex flex-row">
+                            <div className="h6 pd-h6">Contact No. #1</div>
+                            <div className="h6 pd-details">: {guide.contactNumber} </div>
+                        </div>
+                        <div className="d-flex flex-row">
+                            <div className="h6 pd-h6">Home Adress</div>
+                            <div className="h6 pd-details">: {guide.address} </div>
+                        </div>
+                        <div className="d-flex mt-2 mr-auto pd-edit-btn-div"><button className="pd-edit-btn">Edit</button></div>
                     </div>
-                    <div className="d-flex flex-row">
-                        <div className="h6 pd-h6">E-mail</div>
-                        <div className="h6 pd-details">: msdperera@gmail.com</div>
-                    </div>
-                    <div className="d-flex flex-row">
-                        <div className="h6 pd-h6">NIC</div>
-                        <div className="h6 pd-details">: #########V</div>
-                    </div>
-                    <div className="d-flex flex-row">
-                        <div className="h6 pd-h6">Contact No. #1</div>
-                        <div className="h6 pd-details">: 072######</div>
-                    </div>
-                    <div className="d-flex flex-row">
-                        <div className="h6 pd-h6">Contact No. #2</div>
-                        <div className="h6 pd-details">: 078######</div>
-                    </div>
-                    <div className="d-flex flex-row">
-                        <div className="h6 pd-h6">Home Adress</div>
-                        <div className="h6 pd-details">: 23, ABC Rd, Galle</div>
-                    </div>
-                    <div className="d-flex mt-2 mr-auto pd-edit-btn-div"><button className="pd-edit-btn">Edit</button></div>
                 </div>
             </div>
-        </div>
-    );
+        );
+    }
+
+    return (
+        <>
+            {displayGuide(props)}
+        </>
+    )
+
+   
 }
 
 export default ProfileDetails;
