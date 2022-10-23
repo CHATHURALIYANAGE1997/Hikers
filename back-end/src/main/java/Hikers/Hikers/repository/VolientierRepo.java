9c0a9a52083491da091efe3a5e0a88cf2ee59236
Chathura Liyanage <84259289+CHATHURALIYANAGE1997@users.noreply.github.com>
2022-10-23 22:08:54 +0530

    Add files via upload

---
+++ back-end/src/main/java/Hikers/Hikers/repository/VolientierRepo.java
@@ -0,0 +1,12 @@
+package Hikers.Hikers.repository;
+
+
+
+import Hikers.Hikers.model.Volientier;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface VolientierRepo extends JpaRepository<Volientier,Long> {
+    
+}
