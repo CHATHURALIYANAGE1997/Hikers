0787fa5da850409180c52306ebcbd3953dc54f69
Chathura Liyanage <84259289+CHATHURALIYANAGE1997@users.noreply.github.com>
2022-10-24 13:21:53 +0530

    Add files via upload

---
+++ back-end/src/main/java/Hikers/Hikers/model/Article.java
@@ -0,0 +1,33 @@
+package Hikers.Hikers.model;
+
+
+import lombok.Data;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Data
+@Entity
+@DynamicUpdate
+@DynamicInsert
+@Table(name = "article")
+public class Article {
+
+    private  static  final long serialVersionUID=1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private Long id;
+
+    private String mountname;
+
+    private String province;
+
+    private String description;
+
+    private Date date;
+
+}
