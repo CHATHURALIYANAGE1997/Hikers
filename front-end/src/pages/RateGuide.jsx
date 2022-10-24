da1ea4290ce31d2f9fb1e13d52c7fab9da36aae2
Avishi <avijayaweera1@gmail.com>
2022-10-24 14:10:20 +0530

    making rate guide

--- front-end/src/pages/RateGuide.jsx
+++ front-end/src/pages/RateGuide.jsx
@@ -1,15 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import "../components/Rating/rateTransport.css"
 import Form from 'react-bootstrap/Form';
 import RateTripinfor from "../components/Rating/RateTripinfor";
-import Securityoption from "../components/Rating/Securityoption";
-import Punctualityoption from "../components/Rating/Punctualityoption";
-import Overallservicequalityoption from "../components/Rating/Overallservicequality";
-import Communicationoption from "../components/Rating/Communication";
-import Domainknowledege from "../components/Rating/Domainknowledge";
-import Rateoptions from "../components/Rating/Rateoptions";
 
 const rateGuide = () => {
+    const [security, setSecurity] = useState();
+    const [punctuality, setPunctuality] = useState();
+    const [communication, setNavigationcapacity] = useState();
+    const [domainknowledge, setCleanliness] = useState();
+    const [overallservicequality, setOverallservicequality] = useState();
     return (
         <div>
             <div className="mt-5 p-5 text-left signupcontainer">
@@ -19,32 +18,67 @@ const rateGuide = () => {
                             <h2 className="transporthead">We appreciate your feedback!</h2>
                             <h5 className="transportsubhead">Rate the Travel Guider</h5>
                             <hr />
-                            <RateTripinfor />
-                            <hr />
+                            {/* <RateTripinfor /> */}
                             <div className="mb-3">
                                 <h5>Security</h5>
-                                {/* <Securityoption /> */}
-                                <Rateoptions />
+                                <div className="Ratingoptionstransport">
+                                    <h1>{security}</h1>
+                                    <input type="radio" name="security" value="Excellent" />Excellent
+                                    {/* onChange={e => setNavigationcapacity(e.target.value)} */}
+                                    <input type="radio" name="security" value="Very Goood" />Very Good
+                                    <input type="radio" name="security" value="Good" />Good
+                                    <input type="radio" name="security" value="Average" />Average
+                                    <input type="radio" name="security" value="Poor" />Poor
+                                    <input type="radio" name="security" value="Very Poor" />Very Poor
+                                </div>
                             </div>
                             <div className="mb-3">
                                 <h5>Punctuality</h5>
-                                {/* <Punctualityoption /> */}
-                                <Rateoptions />
+                                <div className="Ratingoptionstransport">
+                                    <h1>{punctuality}</h1>
+                                    <input type="radio" name="punctuality" value="Excellent" />Excellent
+                                    <input type="radio" name="punctuality" value="Very Goood" />Very Good
+                                    <input type="radio" name="punctuality" value="Good" />Good
+                                    <input type="radio" name="punctuality" value="Average" />Average
+                                    <input type="radio" name="punctuality" value="Poor" />Poor
+                                    <input type="radio" name="punctuality" value="Very Poor" />Very Poor
+                                </div>
                             </div>
                             <div className="mb-3">
                                 <h5>Communication</h5>
-                                {/* <Communicationoption /> */}
-                                <Rateoptions />
+                                <div className="Ratingoptionstransport">
+                                    <h1>{communication}</h1>
+                                    <input type="radio" name="communication" value="Excellent" />Excellent
+                                    <input type="radio" name="communication" value="Very Goood" />Very Good
+                                    <input type="radio" name="communication" value="Good" />Good
+                                    <input type="radio" name="communication" value="Average" />Average
+                                    <input type="radio" name="communication" value="Poor" />Poor
+                                    <input type="radio" name="communication" value="Very Poor" />Very Poor
+                                </div>
                             </div>
                             <div className="mb-3">
                                 <h5>Domain Knowledge</h5>
-                                {/* <Domainknowledege /> */}
-                                <Rateoptions />
+                                <div className="Ratingoptionstransport">
+                                    <h1>{domainknowledge}</h1>
+                                    <input type="radio" name="domainknowledge" value="Excellent" />Excellent
+                                    <input type="radio" name="domainknowledge" value="Very Goood" />Very Good
+                                    <input type="radio" name="domainknowledge" value="Good" />Good
+                                    <input type="radio" name="domainknowledge" value="Average" />Average
+                                    <input type="radio" name="domainknowledge" value="Poor" />Poor
+                                    <input type="radio" name="domainknowledge" value="Very Poor" />Very Poor
+                                </div>
                             </div>
                             <div className="mb-3">
                                 <h5>Overall Service Quality</h5>
-                                {/* <Overallservicequalityoption /> */}
-                                <Rateoptions />
+                                <div className="Ratingoptionstransport">
+                                    <h1>{overallservicequality}</h1>
+                                    <input type="radio" name="overallservicequality" value="Excellent" />Excellent
+                                    <input type="radio" name="overallservicequality" value="Very Goood" />Very Good
+                                    <input type="radio" name="overallservicequality" value="Good" />Good
+                                    <input type="radio" name="overallservicequality" value="Average" />Average
+                                    <input type="radio" name="overallservicequality" value="Poor" />Poor
+                                    <input type="radio" name="overallservicequality" value="Very Poor" />Very Poor
+                                </div>
                             </div>
                             <div className="mb-3">
 
