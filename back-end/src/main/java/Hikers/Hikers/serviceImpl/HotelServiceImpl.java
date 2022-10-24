e3e22de011eb0f30eca8af1a4677e748498d9706
Chathura Liyanage <84259289+CHATHURALIYANAGE1997@users.noreply.github.com>
2022-10-24 22:35:38 +0530

    Update HotelServiceImpl.java

--- back-end/src/main/java/Hikers/Hikers/serviceImpl/HotelServiceImpl.java
+++ back-end/src/main/java/Hikers/Hikers/serviceImpl/HotelServiceImpl.java
@@ -12,6 +12,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.*;
 import java.util.Optional;
 
@@ -39,6 +41,9 @@ public class HotelServiceImpl implements HotelService {
 
     @Autowired
     private JwtFilter jwtFilter;
+
+    @Autowired
+    private UserRepo userRepo;
 
     @Autowired
     private PasswordEncoder passwordEncoder;
@@ -172,6 +177,54 @@ public class HotelServiceImpl implements HotelService {
             if(! hotelbooking.isEmpty()){
                 return new ResponseEntity(hotelbooking, HttpStatus.OK);
             } else {
+                return new ResponseEntity(new ArrayList<>(), HttpStatus.BAD_REQUEST);
+            }
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+        return new ResponseEntity(new ArrayList<>(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @Override
+    public ResponseEntity<?> hotelbooking(Map<String, String> requestMap) {
+        try{
+            if(!Objects.isNull(requestMap)){
+                Hotelbooking hotelbooking=new Hotelbooking();
+                hotelbooking.setHotel_id(Long.parseLong(requestMap.get("hotelid")));
+                User user=userRepo.findByEmail(jwtFilter.getCurrentUser());
+                hotelbooking.setUser_id(user.getId());
+                hotelbooking.setFirstname(user.getFirstname());
+                hotelbooking.setLastname(user.getLastname());
+                Date date = Calendar.getInstance().getTime();
+                DateFormat dateFormat = new SimpleDateFormat("yyyy-mm-dd");
+                String strDate = dateFormat.format(date);
+                hotelbooking.setDate(strDate);
+                hotelbooking.setCrowd(requestMap.get("crowd"));
+                String sin="Single bed";
+                String dou="Double bed";
+                String tre="Trible bed";
+                Hotelroom hotelroom=hotelroomRepo.findByHotel_idAndRoom_type(requestMap.get("hotel_id"),sin);
+                Hotelroom hotelroom1=hotelroomRepo.findByHotel_idAndRoom_type(requestMap.get("hotel_id"),dou);
+                Hotelroom hotelroom2=hotelroomRepo.findByHotel_idAndRoom_type(requestMap.get("hotel_id"),tre);
+                int singlebedroomprice=Integer.parseInt(hotelroom.getAmount());
+                int doublebedroomprice=Integer.parseInt(hotelroom1.getAmount());
+                int triblebedroomprice=Integer.parseInt(hotelroom2.getAmount());
+                int siglebedcount=Integer.parseInt(requestMap.get("Singlebed"));
+                int doublebedcount=Integer.parseInt(requestMap.get("Doublebed"));
+                int triblebedcount=Integer.parseInt(requestMap.get("Triblebed"));
+
+                int totalamount=singlebedroomprice*siglebedcount+doublebedroomprice*doublebedcount+triblebedcount*triblebedroomprice;
+                hotelbooking.setPayment(String.valueOf(totalamount));
+                hotelbooking.setPayment(requestMap.get("payment"));
+                hotelbooking.setCheckindate(requestMap.get("checkindate"));
+                hotelbooking.setCheckoutdate(requestMap.get("checkoutdate"));
+                hotelbooking.setStatus("false");
+                hotelbooking.setSingalbed(String.valueOf(siglebedcount));
+                hotelbooking.setDoublebed(String.valueOf(doublebedcount));
+                hotelbooking.setTriblebed(String.valueOf(triblebedcount));
+                hotelbookingRepo.save(hotelbooking);
+                return Hutils.getResponseEntity("second part successfully", HttpStatus.OK);
+            }else {
                 return new ResponseEntity(new ArrayList<>(), HttpStatus.BAD_REQUEST);
             }
         }catch (Exception ex){
