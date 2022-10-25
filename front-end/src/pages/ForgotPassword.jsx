114f67060273ea7fecb10337869e6147d7719311
Chathura Liyanage <84259289+CHATHURALIYANAGE1997@users.noreply.github.com>
2022-10-25 16:33:14 +0530

    Update ForgotPassword.jsx

--- front-end/src/pages/ForgotPassword.jsx
+++ front-end/src/pages/ForgotPassword.jsx
@@ -1,15 +1,49 @@
-import React from "react"
+import React, {useState} from "react"
 import signin from "../components/Signin/signin.css"
 import forgotpassword from "../components/Signin/forgotpassword.png";
 import Navbar from "../components/Navbar/Navbar";
 import { useTitle } from "../components/Title/Title"
+import {useDispatch} from "react-redux";
+import {nav} from "../services/user/userActions";
+import {Alert} from "react-bootstrap";
 
 
 const ForgotPassword = (props) => {
 
-    const nav=()=>{
-        return props.history("/resetpassword");
-    }
+
+        const [error, setError] = useState();
+        const [show, setShow] = useState(true);
+
+        const initialState = {
+            email: ""
+        };
+
+        const [user, setUser] = useState(initialState);
+        const credentialChange = (event) => {
+            const { name, value } = event.target;
+            setUser({ ...user, [name]: value });
+
+        };
+
+        const dispatch = useDispatch();
+
+    const nav1 = () => {
+        dispatch(nav(user)).then((response) => {
+            resetLoginForm();
+            setShow(true);
+            setError("Check Your Email")
+        }).catch((error) => {
+            console.log(error.message);
+            setShow(true);
+            resetLoginForm();
+            setError("Invalid data");
+        });
+    };
+
+    const resetLoginForm = () => {
+        setUser(initialState);
+    };
+
     useTitle("Hikers")
 
     return (
@@ -24,15 +58,25 @@ const ForgotPassword = (props) => {
                         <div className="login-body">
                             <h1 className="forgotpasswordh1">Forgot Password? </h1>
                             <form>
+                                {show && props.message && (
+                                    <Alert variant="success" onClose={() => setShow(false)} dismissible>
+                                        {props.message}
+                                    </Alert>
+                                )}
+                                {show && error && (
+                                    <Alert variant="danger" onClose={() => setShow(false)} dismissible>
+                                        {error}
+                                    </Alert>
+                                )}
                             <p className="forgotpasswordp">Enter the email-address associated with your account</p>
                                 <div class="forgotpasswordform">
-                                    <label for="email">Email Address</label>
+                                    <label type={"email"}  name="email" >Email Address</label>
                                     <br/>
-                                    <input type="text" class="form-control forgotpasswordemail" id="email"/>
+                                    <input type={"email"} value={user.email} onChange={credentialChange} name="email" class="form-control forgotpasswordemail" id="email"/>
                                 </div>
                                 <br/>
                                 <div class="col-6 mt-3 mx-auto text-center loginbtn">
-                                    <button onClick={nav} type="submit" class="btn btn-primary">Reset Password</button>
+                                    <button  type="submit" type="button" onClick={nav1} class="btn btn-primary">Reset Password</button>
                                 </div>
                             </form>
                         </div>
