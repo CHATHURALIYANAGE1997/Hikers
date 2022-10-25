b6fe33fbeb8a7af3f33a26ddaaea58f4caa2ec10
Avishi <avijayaweera1@gmail.com>
2022-10-25 16:43:23 +0530

    changing css in valuable customers

--- front-end/src/components/Admin/Home/ValuableCustomer.jsx
+++ front-end/src/components/Admin/Home/ValuableCustomer.jsx
@@ -15,40 +15,40 @@ const ValuableCustomer = () => {
             <div className="d-flex flex-row justify-content-between valuable-customers">
                 <span className="popular-places-province vc-bold">Image</span>
                 <span className="popular-places-province vc-bold">Name</span>
-                <span className="popular-places-province vc-bold">Spent Amount</span>
+                <span className="popular-places-province vc-bold">Trips count</span>
                 <span className="popular-places-province vc-bold">Promotion</span>
             </div>
-            <div className="d-flex flex-row justify-content-between valuable-customers">
+            <div className="d-flex flex-row w-25 valuable-customers">
                 <img src={avishi} className="valuable-customer-profile-img" alt="Profile Picture"></img>
                 <span className="popular-places-province">Avishi Jayaweera</span>
                 <span className="popular-places-province">LKR 125 400</span>
                 <span className="promotion-applied">Applied</span>
             </div>
-            <div className="d-flex flex-row justify-content-between valuable-customers">
+            <div className="d-flex flex-row w-25 valuable-customers">
                 <img src={kasun} className="valuable-customer-profile-img" alt="Profile Picture"></img>
                 <span className="popular-places-province">Kasun Perera</span>
                 <span className="popular-places-province">LKR 118 000</span>
                 <span className="promotion-notapplied">Not applied</span>
             </div>
-            <div className="d-flex flex-row justify-content-between valuable-customers">
+            <div className="d-flex flex-row w-25 valuable-customers">
                 <img src={pramith} className="valuable-customer-profile-img" alt="Profile Picture"></img>
                 <span className="popular-places-province">Chathura Liyanage</span>
                 <span className="popular-places-province">LKR 46 560</span>
                 <span className="promotion-notapplied">Not applied</span>
             </div>
-            <div className="d-flex flex-row justify-content-between valuable-customers">
+            <div className="d-flex flex-row w-25 valuable-customers">
                 <img src={sadun} className="valuable-customer-profile-img" alt="Profile Picture"></img>
                 <span className="popular-places-province">Sadun Kamilka</span>
                 <span className="popular-places-province">LKR 37 900</span>
                 <span className="promotion-notapplied">Not applied</span>
             </div>
-            <div className="d-flex flex-row justify-content-between valuable-customers">
+            <div className="d-flex flex-row w-25 valuable-customers">
                 <img src={chathura} className="valuable-customer-profile-img" alt="Profile Picture"></img>
                 <span className="popular-places-province">Pramith Perera</span>
                 <span className="popular-places-province">LKR 35 500</span>
                 <span className="promotion-notapplied">Not applied</span>
             </div>
-            <div className="d-flex flex-row justify-content-between valuable-customers">
+            <div className="d-flex flex-row w-25 valuable-customers">
                 <img src={subha} className="valuable-customer-profile-img" alt="Profile Picture"></img>
                 <span className="popular-places-province">Subha Jayaweera</span>
                 <span className="popular-places-province">LKR 29 500</span>
