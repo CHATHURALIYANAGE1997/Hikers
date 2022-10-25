2e9009aa9ed9a0e95bdf8f3b490783220e1bf8ae
Subhagya Jayaweera <62256388+jayaweera2@users.noreply.github.com>
2022-10-25 17:11:22 +0530

    Update LandingPage.jsx

--- front-end/src/pages/LandingPage.jsx
+++ front-end/src/pages/LandingPage.jsx
@@ -46,6 +46,20 @@ const LandingPageNew = () => {
             <Articlecarousel />
           </div>
         </div>
+        <div className="Container3">
+          <div className="row aboutUsrow">
+            <h1 className="AboutUsTitle">About Us</h1>
+            <p className="fw-bold AboutUsParagraph">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book. It has
+              survived not only five centuries,but also the leap into electronic
+              typesetting, remaining essentially unchanged. It was popularised
+              in
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
