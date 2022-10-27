05855655f63522e68bc9dab7626594b5238b32de
Subhagya Jayaweera <62256388+jayaweera2@users.noreply.github.com>
2022-10-27 12:51:55 +0530

    Update LandingPage.jsx

--- front-end/src/pages/LandingPage.jsx
+++ front-end/src/pages/LandingPage.jsx
@@ -1,79 +1,79 @@
-import React from "react";
-import Navbar from "../components/Navbar/Navbar";
-import { Container, Image } from "react-bootstrap";
-import { Row, Col } from "react-bootstrap";
-import "../components/Landing/Landing.css";
-import Couple from "../components/Landing/couple.png";
-import Articlecarousel from "../components/Carousel/carousel";
-import LandingFooter from "../components/Footer/footer";
+// import React from "react";
+// import Navbar from "../components/Navbar/Navbar";
+// import { Container, Image } from "react-bootstrap";
+// import { Row, Col } from "react-bootstrap";
+// import "../components/Landing/Landing.css";
+// import Couple from "../components/Landing/couple.png";
+// import Articlecarousel from "../components/Carousel/carousel";
+// import LandingFooter from "../components/Footer/footer";
 
-const LandingPageNew = () => {
-  return (
-    <div>
-      <div>
-        <Navbar />
-      </div>
-      <div>
-        {/* <div className="LandingContainer">
-          <Row className="LandingRow">
-            <Col sm={5}>
-              <h1 className="hikingTitle">Welcome To Hikers</h1>
-              <h3>Safe Trip to the peak</h3>
-            </Col>
-            <Col sm={7}>
-              <img src={LandingWall1} className="img-fluid"></img>
-            </Col>
-          </Row>
-        </div> */}
-        <div class="landingContainer">
-          <div class="row landing-row">
-            <div class="col landing-col">
-              <h1 className="hikingTitle">Welcome To Hikers</h1>
-              <h4 className="hikingTitleSub">
-                <i>Safe trip to the pea</i>k
-              </h4>
-              <div className="aboutUsDetailsDiv">
-                <h6 className="aboutUsDetails">
-                  Hikers provide you the best hiking experience in Sri Lanka
-                  Which facilitates the travelers to book flexible dates for
-                  hiking by having the optional to get
-                  accomadation,transportation service and guidance through the
-                  app which would help the travelers to get many adventurous
-                  experiance.
-                </h6>
-              </div>
-            </div>
-            <div class="col landing-col1">
-              <img
-                src={Couple}
-                className="rounded img-fluid landingImage"
-              ></img>
-            </div>
-          </div>
-        </div>
-        <div className="Container2">
-          <div className="row articlecarouselrow">
-            <Articlecarousel />
-          </div>
-        </div>
-        <div className="Container3">
-          <div className="row aboutUsrow">
-            <h1 className="AboutUsTitle">About Us</h1>
-            <p className="fw-bold AboutUsParagraph">
-               Hikers is a newly generated website which was made to overcome
-               the problems faced by the traditional method which was used for
-               hiking.The attractive and simple website will convert the manual
-               process of hiking into a web application which would facilitate the hikers to do 
-               their hiking  easily and in a secure way. 
-            </p>
-          </div>
-        </div>
-        <div className="Container4">
-          <LandingFooter />
-        </div>
-      </div>
-    </div>
-  );
-};
-export default LandingPageNew;
+// const LandingPageNew = () => {
+//   return (
+//     <div>
+//       <div>
+//         <Navbar />
+//       </div>
+//       <div>
+//         {/* <div className="LandingContainer">
+//           <Row className="LandingRow">
+//             <Col sm={5}>
+//               <h1 className="hikingTitle">Welcome To Hikers</h1>
+//               <h3>Safe Trip to the peak</h3>
+//             </Col>
+//             <Col sm={7}>
+//               <img src={LandingWall1} className="img-fluid"></img>
+//             </Col>
+//           </Row>
+//         </div> */}
+//         <div class="landingContainer">
+//           <div class="row landing-row">
+//             <div class="col landing-col">
+//               <h1 className="hikingTitle">Welcome To Hikers</h1>
+//               <h4 className="hikingTitleSub">
+//                 <i>Safe trip to the pea</i>k
+//               </h4>
+//               <div className="aboutUsDetailsDiv">
+//                 <h6 className="aboutUsDetails">
+//                   Hikers provide you the best hiking experience in Sri Lanka
+//                   Which facilitates the travelers to book flexible dates for
+//                   hiking by having the optional to get
+//                   accomadation,transportation service and guidance through the
+//                   app which would help the travelers to get many adventurous
+//                   experiance.
+//                 </h6>
+//               </div>
+//             </div>
+//             <div class="col landing-col1">
+//               <img
+//                 src={Couple}
+//                 className="rounded img-fluid landingImage"
+//               ></img>
+//             </div>
+//           </div>
+//         </div>
+//         <div className="Container2">
+//           <div className="row articlecarouselrow">
+//             <Articlecarousel />
+//           </div>
+//         </div>
+//         <div className="Container3">
+//           <div className="row aboutUsrow">
+//             <h1 className="AboutUsTitle">About Us</h1>
+//             <p className="fw-bold AboutUsParagraph">
+//                Hikers is a newly generated website which was made to overcome
+//                the problems faced by the traditional method which was used for
+//                hiking.The attractive and simple website will convert the manual
+//                process of hiking into a web application which would facilitate the hikers to do 
+//                their hiking  easily and in a secure way. 
+//             </p>
+//           </div>
+//         </div>
+//         <div className="Container4">
+//           <LandingFooter />
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+// export default LandingPageNew;
 
