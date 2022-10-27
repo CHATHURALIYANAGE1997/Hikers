021db789eed1e917c1888a3918479efbb9856d22
Subhagya Jayaweera <62256388+jayaweera2@users.noreply.github.com>
2022-10-27 12:24:38 +0530

    Update TodayTrips.jsx

--- front-end/src/components/Traveler/Trips/TodayTrips.jsx
+++ front-end/src/components/Traveler/Trips/TodayTrips.jsx
@@ -8,10 +8,10 @@ const TodayTrips = () => {
         <div class="today-container">
             <div className="d-flex flex-column today-card">
                 <div className="d-flex flex-row justify-content-between">
-                    <h5 className="today-title">Today</h5>
-                    <h7 className="today-date">27th July 2022</h7>
+                    <h5 className="today-title">Upcoming</h5>
+                    <h7 className="today-date">26th Octomber 2022</h7>
                 </div>
-                <h7>Starting : </h7>
+                <h7>Starting : 6.00 A.M </h7>
                 <h7>Ending : </h7>
                 <h3 className="today-trip">Trip to Hanthana</h3>
                 <div className="today-transport mb-3 d-flex flex-row">
